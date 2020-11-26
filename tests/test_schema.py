dce7e545477153e3bc4f15629d089713618dd9a4
Jacob Hayes <jacob.r.hayes@gmail.com>
2020-11-26 14:48:26 -0500

    Add test with complex annotation with a model instance

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,6 +1,14 @@
 from typing import Annotated, get_origin, get_type_hints
 
-from pydantic_annotated import Alias, BaseModel, Default, _pydantic_is_annotated_aware
+from pydantic_annotated import (
+    GE,
+    Alias,
+    BaseModel,
+    Default,
+    Description,
+    FieldAnnotation,
+    _pydantic_is_annotated_aware,
+)
 
 
 def test_model():
@@ -20,3 +28,39 @@ def test_nested_annotations():
         x: Annotated[Annotated[int, Default(5)], Default(10)]
 
     assert Model().x == 10
+
+
+def test_complex_annotations():
+    PII = FieldAnnotation.named("PII")
+    # Do we want to support linking a model/type with an annotation?
+    ComplexAnnotation = FieldAnnotation.named("complex_annotation")
+
+    class ComplexModel(BaseModel):
+        x: int
+        y: int
+
+    class FooBar(BaseModel):
+        count: Annotated[
+            int,
+            Alias("cnt"),
+            Description("Count of FooBars"),
+            GE(0),
+            PII(False),
+            ComplexAnnotation(ComplexModel(x=1, y=2)),
+        ]
+
+    assert FooBar.schema() == {
+        "title": "FooBar",
+        "type": "object",
+        "properties": {
+            "cnt": {
+                "title": "Cnt",
+                "description": "Count of FooBars",
+                "minimum": 0,
+                "PII": False,
+                "complex_annotation": ComplexModel(x=1, y=2),
+                "type": "integer",
+            }
+        },
+        "required": ["cnt"],
+    }
